973bb823021ee6c55f32491d3560639461b477c3
Privalov94 <arprivalov110594@gmail.com>
2022-09-14 20:53:34 +0500

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader, 'css-loader',
