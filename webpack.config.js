8caceae5cf1536144421e509e375e9092485aca5
Privalov94 <arprivalov110594@gmail.com>
2022-09-14 20:48:14 +0500

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader, 'css-loader',
